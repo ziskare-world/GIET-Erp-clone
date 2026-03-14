594790f6ed1c2960902d3e2e0ab33b06708d8529
Ayushman23k <ayushmanmeher65@mail.com>
2026-03-14 18:51:38 +0530

    Publish v1.1 update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.gieterp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
