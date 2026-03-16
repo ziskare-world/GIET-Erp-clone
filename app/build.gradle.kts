d978e3f374a10f707657451bbaf914ff7f96292a
Ayushman23k <ayushmanmeher65@mail.com>
2026-03-16 09:35:08 +0530

    Publish v1.6 test update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.gieterp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
